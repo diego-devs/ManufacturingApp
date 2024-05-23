0db5b603f01c95fe2625dd3feeb6d090a8635bec
Diego Díaz Mendoza <d_diazmendoza@hotmail.com>
2024-05-22 18:04:28 -0600

    Add logger to repository service

--- ManufacturingApp.API/Data/ManufacturingRepository.cs
+++ ManufacturingApp.API/Data/ManufacturingRepository.cs
@@ -1,5 +1,7 @@
 ﻿using ManufacturingApp.API.Interfaces;
 using ManufacturingApp.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ManufacturingApp.API.Data
@@ -7,57 +9,101 @@ namespace ManufacturingApp.API.Data
     public class ManufacturingRepository<T> : IManufacturingRepository<T> where T : class
     {
         // Concrete implementation
-
         private readonly ManufacturingContext _manufacturingContext;
         private readonly DbSet<T> _dbSets;
+        private readonly ILogger _logger;
 
-        public ManufacturingRepository(ManufacturingContext manufacturingContext)
+        public ManufacturingRepository(ManufacturingContext manufacturingContext, ILogger logger)
         {
             _manufacturingContext = manufacturingContext;
             _dbSets = _manufacturingContext.Set<T>();
+            _logger = logger;
         }
 
         public async Task<ICollection<T>> GetAllAsync()
         {
-            // get all elements from entity from db
-            return await _dbSets.ToListAsync();
+            try
+            {
+                // get all elements from entity from db
+                return await _dbSets.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while getting all entities");
+                throw;
+            }
+            
         }
         public async Task<T> GetAsync(int id)
         {
-            var entity = await _dbSets.FindAsync(id);
-            if (entity != null) 
+            // get one entity from context by id
+            try
             {
-                return entity;
-            } 
-            else
+                var entity = await _dbSets.FindAsync(id);
+                if (entity != null)
+                {
+                    return entity;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
             {
-                throw new NullReferenceException();
+                _logger.LogError(ex, "An error occurred when getting the entity");
+                throw;
             }
         }
 
         public async void CreateAsync(T entity)
         {
-            // context.Add to database
-            // save changes
-            await _dbSets.AddAsync(entity);
-            await _manufacturingContext.SaveChangesAsync();
-            
+            try
+            {
+                // context.Add to database
+                // save changes
+                await _dbSets.AddAsync(entity);
+                await _manufacturingContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred when creating the entity");
+                throw;
+            }
         }
        
        public async void UpdateAsync(T entity)
         {
-            _dbSets.Update(entity);
-            await _manufacturingContext.SaveChangesAsync();
+            // update entity 
+            try
+            {
+                _dbSets.Update(entity);
+                await _manufacturingContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred when updating the entity");
+                throw;
+            }
         }
         public async void DeleteAsync(int id)
         {
-            // context delete from database
-            // save changes
-            // return true
-            var entity = await GetAsync(id);
-            _dbSets.Remove(entity);
-            await _manufacturingContext.SaveChangesAsync();
-
+            try
+            {
+                // context delete from database
+                // save changes
+                var entity = await GetAsync(id);
+                if (entity != null)
+                {
+                    _dbSets.Remove(entity);
+                    await _manufacturingContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred when deleting the entity");
+                throw;
+            }
         }
     }
 }
--- ManufacturingApp.API/Program.cs
+++ ManufacturingApp.API/Program.cs
@@ -6,15 +6,15 @@ using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
 // Add DB Context
 builder.Services.AddDbContext<ManufacturingContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add repository service 
 builder.Services.AddScoped(typeof(IManufacturingRepository<>), typeof(ManufacturingRepository<>));
-
+// Add logger
+builder.Services.AddLogging();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
